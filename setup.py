fb30279b32c97e2bad463b9edb033e34daedd66a
Ignacio Alonso <ignacio.alley@gmail.com>
2024-01-04 23:05:43 -0600

    Update setup.py with package metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='aig',
     version='0.1',
+    author='Ignacio Alonso',
+    author_email='ignacio.alley@gmail.com',
+    description='A CLI tool to automatically generate git commit messages using OpenAI GPT-4',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nachoal/aig',
     packages=find_packages(),
     install_requires=[
         'GitPython',  # For interacting with Git
@@ -14,4 +23,17 @@ setup(
             'aig = aig.main:main',
         ],
     },
+     classifiers=[
+        'Development Status :: 3 - Alpha',  # Change to '5 - Production/Stable' if applicable
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Version Control :: Git',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.6',
 )
